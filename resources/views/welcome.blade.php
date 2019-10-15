bcdbd424527462b808bbebdda5e59ecd73bc210b
Bintang Lazuardi <bintanglazuardi@mail.ugm.ac.id>
2019-10-15 13:43:38 +0700

    changed 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Bintang Lazuardi
                 </div>
 
                 <div class="links">
